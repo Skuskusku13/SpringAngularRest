ef9a4787573e3e0be79ebceffedeb44f07221a01
Skuskusku13 <danlevy149@gmail.com>
2023-12-11 21:32:45 +0100

    create components users vehicules and add navbar

--- frontRestProject/src/app/app.module.ts
+++ frontRestProject/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {HttpClientModule} from "@angular/common/http";
+import { UsersComponent } from './users/users.component';
+import { VehiculesComponent } from './vehicules/vehicules.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UsersComponent,
+    VehiculesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

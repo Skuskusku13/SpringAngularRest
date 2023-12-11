644bcd8b9bde2ae377e6b594117d780e5c386787
Skuskusku13 <danlevy149@gmail.com>
2023-12-11 22:17:53 +0100

    mise en place de l'environnement de l'api rest

--- myRestProject/src/main/java/com/example/myRestProject/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.myRestProject.repositories;
-
-import com.example.myRestProject.models.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-
-}
